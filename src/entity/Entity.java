ef13b53abe780136c9eeded18448b21440c6d3c9
Fabio.Marconi <fmarconi@unibz.it>
2022-05-10 11:22:35 +0200

    Collisiom works

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -3,6 +3,7 @@ package entity;
 import Main.GamePanel;
 
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 
 // parent class => Whatever entity moves
@@ -18,12 +19,12 @@ public abstract class Entity {
     protected int jumpStrenght;
     protected boolean falling = false;
     protected boolean jumping = false;
-    protected Rectangle hitbox;
+    protected Rectangle2D.Float hitbox;
     protected int direction; //direction should be used only to choose which image will be shown, not to establish physics => -1 = sx; 0= center; 1=dx
 
 
     //Constructor
-    public Entity(int x, int y,int speed, int jumpStrenght, int weight, Skin skin) {
+    public Entity(int x, int y, int speed, int jumpStrenght, int weight, Skin skin) {
 
         this.x = x;
         this.y = y;
@@ -34,78 +35,148 @@ public abstract class Entity {
         this.jumpStrenght = jumpStrenght;
         this.weight = weight;
         this.entitySkin = skin;
-        createHitBox();
 
     }
 
-    protected void drawHitBox(Graphics g){
+    protected void drawHitBox(Graphics g) {
         // For debugging purposes
         g.setColor(Color.black);
-        g.drawRect(hitbox.x, hitbox.y, hitbox.width, hitbox.height);
+        g.drawRect((int) hitbox.x, (int) hitbox.y, (int) hitbox.width, (int) hitbox.height);
 
     }
 
-    private void createHitBox() {
-
-       hitbox = new Rectangle(x,y,48,48);
-
+    protected void createHitBox(float x, float y, float width, float height) {
+        hitbox = new Rectangle2D.Float(x, y, width, height);
     }
 
-    protected void updateHitBox(){
+    protected void updateHitBox() {
         hitbox.x = x;
         hitbox.y = y;
     }
 
-    public Rectangle getHitbox(){
+    public Rectangle2D.Float getHitbox() {
         return hitbox;
     }
 
 
     //Getters
-    public boolean getFalling(){return falling;}
-    public boolean getJumping(){return jumping;}
-    public int getX(){return x;}
-    public int getY(){return y;}
-    public int getXAcc(){return xAcc;}
-    public int getYAcc(){return yAcc;}
-    public int getEntitySpeed(){return entitySpeed;};
-    public float getWeight(){return weight;}
-    public int getJumpStrenght(){return jumpStrenght;}
-    public int getDirection(){return direction;}
-    public Skin getSkin(){return entitySkin;}
-    public BufferedImage getCurrentImage(){return currentImage;}
-    //Setters
-    public void setFalling(boolean isfalling){falling = isfalling;}
-    public void setJumping(boolean isjumping){jumping = isjumping;}
-    public void setX(int newX){ x = newX;}
-    public void setY(int newY){ y = newY;}
-    public void setXAcc(int newXAcc){ xAcc = newXAcc;}
-    public void setYAcc(int newYAcc){ yAcc = newYAcc;}
-    public void setEntitySpeed(int newEntitySpeed){ entitySpeed = newEntitySpeed;}
-    public void setWeight(int newWeight){ weight = newWeight;}
-    public void setJumpStrenght(int newJumpStrenght){ jumpStrenght = newJumpStrenght;}
-    public void setDirection(int newDirection){ direction = newDirection;}
-    public void setSkin(Skin newSkin){ entitySkin = newSkin;}
-    public void setCurrenImage(BufferedImage newImage){currentImage = newImage;}
+    public boolean getFalling() {
+        return falling;
+    }
 
+    public boolean getJumping() {
+        return jumping;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getXAcc() {
+        return xAcc;
+    }
+
+    public int getYAcc() {
+        return yAcc;
+    }
+
+    public int getEntitySpeed() {
+        return entitySpeed;
+    }
+
+    ;
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public int getJumpStrenght() {
+        return jumpStrenght;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public Skin getSkin() {
+        return entitySkin;
+    }
+
+    public BufferedImage getCurrentImage() {
+        return currentImage;
+    }
+
+    //Setters
+    public void setFalling(boolean isfalling) {
+        falling = isfalling;
+    }
+
+    public void setJumping(boolean isjumping) {
+        jumping = isjumping;
+    }
+
+    public void setX(int newX) {
+        x = newX;
+    }
+
+    public void setY(int newY) {
+        y = newY;
+    }
+
+    public void setXAcc(int newXAcc) {
+        xAcc = newXAcc;
+    }
+
+    public void setYAcc(int newYAcc) {
+        yAcc = newYAcc;
+    }
+
+    public void setEntitySpeed(int newEntitySpeed) {
+        entitySpeed = newEntitySpeed;
+    }
+
+    public void setWeight(int newWeight) {
+        weight = newWeight;
+    }
+
+    public void setJumpStrenght(int newJumpStrenght) {
+        jumpStrenght = newJumpStrenght;
+    }
+
+    public void setDirection(int newDirection) {
+        direction = newDirection;
+    }
+
+    public void setSkin(Skin newSkin) {
+        entitySkin = newSkin;
+    }
+
+    public void setCurrenImage(BufferedImage newImage) {
+        currentImage = newImage;
+    }
 
 
     //MOVEMENT METHODS
 
 
-    public void right(){
-         direction = 1;
+    public void right() {
+        direction = 1;
     }
 
-    public void stop(){
+    public void stop() {
         direction = 0;
     }
 
-    public void jump(){
+    public void jump() {
         yAcc -= jumpStrenght;
         jumpStrenght -= weight;
     }
 
-    public void die(){}
+    public void die() {
+    }
 
 }
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,4 +1,5 @@
 package entity;
+
 import Main.Game;
 import Main.GamePanel;
 import Main.KeyHandler;
@@ -11,55 +12,55 @@ import static utilz.HelpMethods.CanMoveHere;
 
 public class Player extends Entity {
 
-    KeyHandler movement = new KeyHandler();
     private Game game;
-    public int centDirection = 1;
-    private int [][] lvlData;
-    private boolean moving = false;
+    private int[][] lvlData;
+    private float xOffset = 4 * Game.scale;
+    private float yOffset = 0;
 
     public Player(int x, int y, int speed, int jumpStrenght, int weight, Skin skin) {
         super(x, y, speed, jumpStrenght, weight, skin);
         this.xAcc = 0;
         this.yAcc = 1;
         this.direction = 1;
+        createHitBox(x, y, 8 * Game.scale, 16 * Game.scale);
     }
 
-    public void update(){
+    public void update() {
         updatePosition();
         updateHitBox();
     }
 
-    public void draw(Graphics g){
-        g.drawImage(getCurrentImage(), getX(), getY(), Game.tileSize, Game.tileSize , null);
-        drawHitBox(g);
+    public void draw(Graphics g) {
+        g.drawImage(getCurrentImage(), (int) (hitbox.x - xOffset), (int) (hitbox.y - yOffset), Game.tileSize, Game.tileSize, null);
+        //drawHitBox(g);
     }
 
-    public void updatePosition(){
+    public void updatePosition() {
         checkGravity();
         setCentDirection();
         movement();
     }
 
-    public void checkGravity(){
-        if (jumping || falling){
+    public void checkGravity() {
+        if (jumping || falling) {
             yAcc += weight;
             System.out.println("sto cadendo");
-        }else{
+        } else {
             yAcc = 0;
         }
     }
 
-    public void setCentDirection(){
-        if (direction == 1){
+    public void setCentDirection() {
+        if (direction == 1) {
             currentImage = entitySkin.center(1);
             //System.out.println("destra");
-        }else{
+        } else {
             currentImage = entitySkin.center(-1);
             //System.out.println("sinistra");
         }
     }
 
-    public void loadLvlData(int[][] lvlData){
+    public void loadLvlData(int[][] lvlData) {
         this.lvlData = lvlData;
     }
 
@@ -67,55 +68,50 @@ public class Player extends Entity {
 
         float xSpeed = 0, ySpeed = 0;
 
-            if (KeyHandler.upPressed) {
-                setJumping(true);
-                yAcc = 1;
-                ySpeed = -entitySpeed;
-                jump();
-                if (direction == 1){
-                    currentImage = entitySkin.jump(1);
-                }else{
-                    currentImage = entitySkin.jump(-1);
-                }
-            } else if (KeyHandler.downPressed) {
-                ySpeed = entitySpeed;
-               // yAcc = 1*weight;
-            } else if (KeyHandler.leftPressed) {
-                xAcc = -1;
-                xSpeed = -entitySpeed;
-                direction = -1;
-                if (jumping || falling){
-                    currentImage = entitySkin.jump(-1);
-                }else{
-                    currentImage = entitySkin.leftAnimation();
-                }
-
-            } else if (KeyHandler.rightPressed) {
-                xAcc = 1;
-                direction = 1;
-                xSpeed = entitySpeed;
-                if (jumping || falling){
-                    currentImage = entitySkin.jump(1);
-                }else{
-                    currentImage = entitySkin.rightAnimation();
-                }
+        if (KeyHandler.upPressed) {
+            setJumping(true);
+            yAcc = 1;
+            ySpeed = -entitySpeed;
+            jump();
+            if (direction == 1) {
+                currentImage = entitySkin.jump(1);
+            } else {
+                currentImage = entitySkin.jump(-1);
             }
-            else{
-                xAcc = 0;
-
+        } else if (KeyHandler.downPressed) {
+            ySpeed = entitySpeed;
+            // yAcc = 1*weight;
+        } else if (KeyHandler.leftPressed) {
+            xAcc = -1;
+            xSpeed = -entitySpeed;
+            direction = -1;
+            if (jumping || falling) {
+                currentImage = entitySkin.jump(-1);
+            } else {
+                currentImage = entitySkin.leftAnimation();
             }
 
-        if (CanMoveHere(x + xSpeed, y + ySpeed, Game.tileSize, Game.tileSize, lvlData)){
-            x += xAcc*entitySpeed;
+        } else if (KeyHandler.rightPressed) {
+            xAcc = 1;
+            direction = 1;
+            xSpeed = entitySpeed;
+            if (jumping || falling) {
+                currentImage = entitySkin.jump(1);
+            } else {
+                currentImage = entitySkin.rightAnimation();
+            }
+        } else {
+            xAcc = 0;
+        }
+
+        if (CanMoveHere(hitbox.x + xSpeed, hitbox.y + ySpeed, hitbox.width, hitbox.height, lvlData)) {
+            x += xAcc * entitySpeed;
             y += yAcc;
         }
 
     }
 
-
-
-
-    public int getSpeed(){
+    public int getSpeed() {
         return entitySpeed;
     }
 }
